fe0a813958217df2f2a53464ba9286ca41b2ec13
XiangLi1412 <63719557+XiangLi1412@users.noreply.github.com>
2020-05-13 19:53:53 +0800

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+// alert("手機請擺橫著看 謝謝!");
+// var lockAllowed = window.screen.lockOrientation("portrait-primary");
+
+
+function open_FB(){
+	window.open("https://www.facebook.com/profile.php?id=100003076867190");
+}
+
+function open_IG(){
+	window.open("https://www.instagram.com/jordan___1127/?hl=zh-tw");
+}
+
+function open_LINE(){
+	window.open("https://line.me/ti/p/QtkEydz-J0");
+}
